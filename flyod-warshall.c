cc9106c499230fbf1c7b6d1ff5167163c0a1ca51
Fin <107251536+swgtds@users.noreply.github.com>
2024-06-09 21:51:15 +0530

    Create flyod-warshall.c

---
+++ flyod-warshall.c
@@ -0,0 +1,104 @@
+/*Algorithm
+
+    1.Initialize the solution matrix same as the input graph matrix as a first step. 
+    2.Then update the solution matrix by considering all vertices as an intermediate vertex. 
+    3.The idea is to pick all vertices one by one and updates all shortest paths which include the picked vertex as an intermediate vertex in the shortest path. 
+    4.When we pick vertex number k as an intermediate vertex, we already have considered vertices {0, 1, 2, .. k-1} as intermediate vertices. 
+    5.For every pair (i, j) of the source and destination vertices respectively, there are two possible cases. 
+        k is not an intermediate vertex in shortest path from i to j. We keep the value of dist[i][j] as it is. 
+        k is an intermediate vertex in shortest path from i to j. We update the value of dist[i][j] as dist[i][k] + dist[k][j], if dist[i][j] > dist[i][k] + dist[k][j]
+  */
+// C Program for Floyd Warshall Algorithm
+#include <stdio.h>
+
+// Number of vertices in the graph
+#define V 4
+
+/* Define Infinite as a large enough
+  value. This value will be used
+  for vertices not connected to each other */
+#define INF 99999
+
+// A function to print the solution matrix
+void printSolution(int dist[][V]);
+
+// Solves the all-pairs shortest path
+// problem using Floyd Warshall algorithm
+void floydWarshall(int dist[][V])
+{
+    int i, j, k;
+
+    /* Add all vertices one by one to
+      the set of intermediate vertices.
+      ---> Before start of an iteration, we
+      have shortest distances between all
+      pairs of vertices such that the shortest
+      distances consider only the
+      vertices in set {0, 1, 2, .. k-1} as
+      intermediate vertices.
+      ----> After the end of an iteration,
+      vertex no. k is added to the set of
+      intermediate vertices and the set
+      becomes {0, 1, 2, .. k} */
+    for (k = 0; k < V; k++) {
+        // Pick all vertices as source one by one
+        for (i = 0; i < V; i++) {
+            // Pick all vertices as destination for the
+            // above picked source
+            for (j = 0; j < V; j++) {
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of
+                // dist[i][j]
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Print the shortest distance matrix
+    printSolution(dist);
+}
+
+/* A utility function to print solution */
+void printSolution(int dist[][V])
+{
+    printf(
+        "The following matrix shows the shortest distances"
+        " between every pair of vertices \n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// driver's code
+int main()
+{
+    /* Let us create the following weighted graph
+            10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3           */
+    int graph[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+
+    // Function call
+    floydWarshall(graph);
+    return 0;
+}
+/*
+Time Complexity analysis
+O(V3), where V is the number of vertices in the graph and we run three nested loops each of size V
+*/
+
